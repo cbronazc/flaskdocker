e9e47fc1e70fc023aeff539997229f044a41d512
Caleb Bron <cbron@azcentral.com>
2014-11-11 11:35:58 -0700

    first commit

---
+++ flocker.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "<html><body><img src='http://i1.kym-cdn.com/entries/icons/original/000/007/447/hello-yes-this-is-dog.png'></body></html>"
+
+if __name__ == '__main__':
+    app.run()
+
